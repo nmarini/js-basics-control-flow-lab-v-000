fc462f482f4d6e6dc9e4be234f17686802a798cf
Nicolas Marini <nmarini44@gmail.com>
2019-04-13 21:49:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,24 +6,19 @@
 // 'No can do.'
 
 function scuberGreetingForFeet(ride) {
-  let greeting;
+  let greeting
   if (ride > 2500) {
-    greeting = 'No can do.';
+    greeting = 'No can do.'
   }
   else if (ride <= 400) {
-    greeting = 'This one is on me!';
+    greeting = 'This one is on me!'
   } else if (ride > 2000) {
-      greeting = 'I will gladly take your thirty bucks.';
+      greeting = 'I will gladly take your thirty bucks.'
   }
-  return greeting;
+  return greeting
 }
 
-function ternaryCheckCity(city) {
-
-}
-
-
-
+function ternaryCheckCity() {
 
 }
 
