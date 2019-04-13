cdd1846415d1d5c6e9242aca8abe49d2e7140b18
Nicolas Marini <nmarini44@gmail.com>
2019-04-13 21:58:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function scuberGreetingForFeet(ride) {
 
 function ternaryCheckCity(city) {
   let response = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  response
 }
 
 function ternaryCheckCity() {
