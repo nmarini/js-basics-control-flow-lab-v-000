aaaf311e03529cca5b26905a5b2b7d4928eba78b
Nicolas Marini <nmarini44@gmail.com>
2019-04-13 21:48:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+// 'gives customers a free sample if the ride is less than or equal to 400 feet'
+// 'This one is on me!'
+// 'charges 30 dollars for a distance over 2000 feet'
+// 'I will gladly take your thirty bucks.'
+// 'does not allow rides over 2500 feet'
+// 'No can do.'
 
 function scuberGreetingForFeet(ride) {
   let greeting;
@@ -14,7 +19,7 @@ function scuberGreetingForFeet(ride) {
 }
 
 function ternaryCheckCity(city) {
-  city === 'NYC' ? 'OK, sounds good.' : 'No go.';
+
 }
 
 
