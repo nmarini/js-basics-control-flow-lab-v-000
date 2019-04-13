43ce253b8dbbe68b6d9067e40940eb09e100797c
Nicolas Marini <nmarini44@gmail.com>
2019-04-13 21:43:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,13 @@
 
 function scuberGreetingForFeet(ride) {
   let greeting
+  if (ride > 2500) {
+    greeting = 'No can do.'
+  }
   if (ride <= 400) {
     greeting = 'This one is on me!'
   } else if (ride > 2000) {
       greeting = 'I will gladly take your thirty bucks.'
-  } else if (ride > 2500) {
-      greeting = 'No can do.'
   }
   return greeting
 }
