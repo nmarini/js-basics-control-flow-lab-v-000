c1131d07f1b5007973010dc6a56ad9e078dcab67
Nicolas Marini <nmarini44@gmail.com>
2019-04-13 21:48:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// 'gives customers a free sample if the ride is less than or equal to 400 feet'
-// 'This one is on me!'
-// 'charges 30 dollars for a distance over 2000 feet'
-// 'I will gladly take your thirty bucks.'
-// 'does not allow rides over 2500 feet'
-// 'No can do.'
+
 
 function scuberGreetingForFeet(ride) {
   let greeting;
