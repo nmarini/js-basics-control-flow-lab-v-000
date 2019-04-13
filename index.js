04bed2917a3f9b93671e67a73cf32a8cb3f77e22
Nicolas Marini <nmarini44@gmail.com>
2019-04-13 22:07:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code in this file!
+// 'gives customers a free sample if the ride is less than or equal to 400 feet'
+// 'This one is on me!'
+// 'charges 30 dollars for a distance over 2000 feet'
+// 'I will gladly take your thirty bucks.'
+// 'does not allow rides over 2500 feet'
+// 'No can do.'
+
+function scuberGreetingForFeet (ride) {
+  let greeting
+  if (ride > 2500) {
+    greeting = 'No can do.'
+  }
+  else if (ride <= 400) {
+    greeting = 'This one is on me!'
+  } else if (ride > 2000) {
+      greeting = 'I will gladly take your thirty bucks.'
+  }
+  return greeting
+}
+
+function ternaryCheckCity (city) {
+   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip (tip) {
+
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+    return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
+}
